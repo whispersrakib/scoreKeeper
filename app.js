3efb28d929271d3da6c36d4dec76f8bc7c39ecb5
rakib <rakibshaikh181@gmail.com>
2022-03-05 08:53:54 +0530

    complete the project

--- app.js
+++ app.js
@@ -1,36 +1,58 @@
-const player1= document.querySelector('#player1Display');
-const player2= document.querySelector('#player2Display');
-const button1 = document.querySelector('#player1');
-const button2 = document.querySelector('#player2');
-const reset = document.querySelector('#reset');
-let p1Score =0;
-let p2Score =0;
-let winningScore=5;
-let isGameOver= false;
-button1.addEventListener('click',()=>{
-    if(!isGameOver){
-        p1Score++;
-        if(p1Score===winningScore){
-            isGameOver=true;
-        }player1.textContent=p1Score;
+const p1 = {
+    score: 0,
+    button: document.querySelector('#p1Button'),
+    display: document.querySelector('#p1Display')
+}
+const p2 = {
+    score: 0,
+    button: document.querySelector('#p2Button'),
+    display: document.querySelector('#p2Display')
+}
+
+const resetButton = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#playto');
+let winningScore = 3;
+let isGameOver = false;
+let IMG = document.querySelector('#img');
+
+function updateScores(player, opponent) {
+    if (!isGameOver) {
+        player.score += 1;
+        if (player.score === winningScore) {
+            isGameOver = true;
+            player.display.classList.add('has-text-success');
+            opponent.display.classList.add('has-text-danger');
+            player.button.disabled = true;
+            opponent.button.disabled = true;
+            IMG.src='https://images.unsplash.com/photo-1569705460033-cfaa4bf9f822?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1937&q=80';
+        }
+        player.display.textContent = player.score;
     }
+}
 
-});
 
-button2.addEventListener('click',()=>{
-    if(!isGameOver){
-        p2Score++;
-        if(p2Score===winningScore){
-            isGameOver=true;
-        }player2.textContent=p2Score;
-    }
-
-});
-
-reset.addEventListener('click',()=>{
-    isGameOver=false;
-    p1Score=0;
-    p2Score=0;
-    player1.textContent=0;
-    player2.textContent=0;
+p1.button.addEventListener('click', function () {
+    updateScores(p1, p2)
 })
+p2.button.addEventListener('click', function () {
+    updateScores(p2, p1)
+})
+
+
+winningScoreSelect.addEventListener('change', function () {
+    winningScore = parseInt(this.value);
+    reset();
+})
+
+resetButton.addEventListener('click', reset)
+
+function reset() {
+    isGameOver = false;
+    for (let p of [p1, p2]) {
+        p.score = 0;
+        p.display.textContent = 0;
+        p.display.classList.remove('has-text-success', 'has-text-danger');
+        p.button.disabled = false;
+        IMG.src='https://images.unsplash.com/photo-1589487391730-58f20eb2c308?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=874&q=80';
+    }
+}
